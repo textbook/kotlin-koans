96abd46966f30f58e5e67564759d937ad34769c5
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-03 17:16:30 +0100

    Complete task vi.41

--- src/vi_generics/n41GenericFunctions.kt
+++ src/vi_generics/n41GenericFunctions.kt
@@ -1,30 +1,16 @@
 package vi_generics
 
-import util.TODO
 import java.util.*
 
-fun task41(): Nothing = TODO(
-    """
-        Task41.
-        Add a 'partitionTo' function that splits a collection into two collections according to a predicate.
-        Uncomment the commented invocations of 'partitionTo' below and make them compile.
-
-        There is a 'partition()' function in the standard library that always returns two newly created lists.
-        You should write a function that splits the collection into two collections given as arguments.
-        The signature of the 'toCollection()' function from the standard library may help you.
-    """,
-        references = { l: List<Int> ->
-            l.partition { it > 0 }
-            l.toCollection(HashSet<Int>())
-        }
-)
+fun <T, C : MutableCollection<T>> Iterable<T>.partitionTo(left: C, right: C, predicate: (T) -> Boolean): Pair<C, C> {
+    val (included, excluded) = this.partition { predicate(it) }
+    return Pair(included.toCollection(left), excluded.toCollection(right))
+}
 
 fun List<String>.partitionWordsAndLines(): Pair<List<String>, List<String>> {
-    task41()
-//    return partitionTo(ArrayList<String>(), ArrayList()) { s -> !s.contains(" ") }
+    return partitionTo(ArrayList(), ArrayList()) { s -> !s.contains(" ") }
 }
 
 fun Set<Char>.partitionLettersAndOtherSymbols(): Pair<Set<Char>, Set<Char>> {
-    task41()
-//    return partitionTo(HashSet<Char>(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
+    return partitionTo(HashSet(), HashSet()) { c -> c in 'a'..'z' || c in 'A'..'Z'}
 }
