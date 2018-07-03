bd5040c68ac5517bea4751b2c0f50df79044aa0c
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-03 14:50:17 +0100

    Complete task ii.15

--- src/ii_collections/n15AllAnyAndOtherPredicates.kt
+++ src/ii_collections/n15AllAnyAndOtherPredicates.kt
@@ -1,39 +1,11 @@
 package ii_collections
 
-fun example2(list: List<Int>) {
+fun Customer.isFrom(city: City): Boolean = this.city == city
 
-    val isZero: (Int) -> Boolean = { it == 0 }
+fun Shop.checkAllCustomersAreFrom(city: City): Boolean = customers.all { it.city == city }
 
-    val hasZero: Boolean = list.any(isZero)
+fun Shop.hasCustomerFrom(city: City): Boolean = customers.any { it.city == city }
 
-    val allZeros: Boolean = list.all(isZero)
+fun Shop.countCustomersFrom(city: City): Int = customers.count { it.city == city }
 
-    val numberOfZeros: Int = list.count(isZero)
-
-    val firstPositiveNumber: Int? = list.firstOrNull { it > 0 }
-}
-
-fun Customer.isFrom(city: City): Boolean {
-    // Return true if the customer is from the given city
-    todoCollectionTask()
-}
-
-fun Shop.checkAllCustomersAreFrom(city: City): Boolean {
-    // Return true if all customers are from the given city
-    todoCollectionTask()
-}
-
-fun Shop.hasCustomerFrom(city: City): Boolean {
-    // Return true if there is at least one customer from the given city
-    todoCollectionTask()
-}
-
-fun Shop.countCustomersFrom(city: City): Int {
-    // Return the number of customers from the given city
-    todoCollectionTask()
-}
-
-fun Shop.findFirstCustomerFrom(city: City): Customer? {
-    // Return the first customer who lives in the given city, or null if there is none
-    todoCollectionTask()
-}
+fun Shop.findFirstCustomerFrom(city: City): Customer? = customers.firstOrNull { it.city == city }
