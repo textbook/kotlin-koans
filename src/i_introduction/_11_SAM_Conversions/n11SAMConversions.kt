c107faa4796875a3e38009f13587d2650e4be853
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2018-07-03 14:37:29 +0100

    More conventional to put lambda outside parentheses?

--- src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
+++ src/i_introduction/_11_SAM_Conversions/n11SAMConversions.kt
@@ -4,6 +4,6 @@ import java.util.*
 
 fun task11(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { x, y -> y - x })
+    Collections.sort(arrayList) { x, y -> y - x }
     return arrayList
 }
